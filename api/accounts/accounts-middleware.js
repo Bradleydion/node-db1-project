9bdac88dba6c88581c70c159e8ce7fbf5034b78e
Bradley Dion <bradleydion@MacBook-Pro.local>
2021-02-24 20:09:34 -0500

    already moving to the middleware. Finished working on accounts model

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,5 +1,11 @@
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  if(!req.body){
+    return res.status(400).json({message:"name and budget are required"})
+  }else if (typeof req.body.name !== 
+    "string" ){
+      return res.status(400).json({message:"name of the account must be a string"})
+    }
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
